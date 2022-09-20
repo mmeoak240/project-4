caba48c754591ea88b3226c862081a05732b1614
Michael Meoak <mmeoak240@gmail.com>
2022-09-19 18:54:04 -0700

    Created an exerciseContainer component

--- client/src/ExerciseContainer.js
+++ client/src/ExerciseContainer.js
@@ -1,1 +1,13 @@
-const ExerciseContainer = () => {};
+import React, { useState, useEffect } from "react";
+
+const ExerciseContainer = () => {
+	const [exercises, setExercises] = useState();
+
+	useEffect(() => {
+		fetch("/exercises")
+			.then((r) => r.json())
+			.then((data) => setExercises(data));
+	}, []);
+};
+
+export default ExerciseContainer;
